5e2553c2d8d25575d2b84e56fbb9d5b56225412a
emmanuel <isiwuemmma@gmail.com>
2025-05-05 11:38:34 +0100

    Initial Commit

---
+++ DayTask/DayTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayTaskApp.swift
+//  DayTask
+//
+//  Created by Emmanuel Isiwu on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DayTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
